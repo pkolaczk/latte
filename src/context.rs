61b96d473a5553b0317a592c1a1d7e2b26b746c8
Piotr Kołaczkowski <pkolaczk@datastax.com>
2023-06-05 14:36:35 +0200

    Update scylla, tokio and rune deps

--- src/context.rs
+++ src/context.rs
@@ -26,7 +26,7 @@ use scylla::frame::response::result::CqlValue;
 use scylla::prepared_statement::PreparedStatement;
 use scylla::transport::errors::{DbError, NewSessionError, QueryError};
 use scylla::transport::session::PoolSize;
-use scylla::{QueryResult, SessionBuilder};
+use scylla::{ExecutionProfile, QueryResult, SessionBuilder};
 use statrs::distribution::Normal;
 use tokio::time::{Duration, Instant};
 use try_lock::TryLock;
@@ -55,12 +55,17 @@ fn ssl_context(conf: &&ConnectionConf) -> Result<Option<SslContext>, CassError> 
 
 /// Configures connection to Cassandra.
 pub async fn connect(conf: &ConnectionConf) -> Result<scylla::Session, CassError> {
+    let profile = ExecutionProfile::builder()
+        .consistency(conf.consistency.scylla_consistency())
+        .request_timeout(Some(Duration::from_secs(60))) // no request timeout
+        .build();
+
     SessionBuilder::new()
         .known_nodes(&conf.addresses)
         .pool_size(PoolSize::PerShard(conf.count))
         .user(&conf.user, &conf.password)
         .ssl_context(ssl_context(&conf)?)
-        .default_consistency(conf.consistency.scylla_consistency())
+        .default_execution_profile_handle(profile.into_handle())
         .build()
         .await
         .map_err(|e| CassError(CassErrorKind::FailedToConnect(conf.addresses.clone(), e)))
